9aebaf060f82dbee3d2428e89cf87976328824c8
Ivan Nikolsky <enty8080@gmail.com>
2021-11-23 23:08:42 +0300

    Update cli.py

--- shreder/cli.py
+++ shreder/cli.py
@@ -45,7 +45,7 @@ class ShrederCLI(Shreder, Badges):
     def start(self):
         if self.args.target and self.args.username and self.args.list:
             if not os.path.exists(self.args.list):
-                self.print_error(f"List file: {self.args.input}: does not exist!")
+                self.print_error(f"List file: {self.args.list}: does not exist!")
                 return
 
             if not self.args.port:
