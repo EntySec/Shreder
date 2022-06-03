3c6b99c5cb4f947be23de156d5cce9e112d0cf6c
Ivan Nikolsky <enty8080@gmail.com>
2022-06-03 14:41:52 +0200

    Add files via upload

--- shreder/__main__.py
+++ shreder/__main__.py
@@ -1,41 +1,54 @@
-#!/usr/bin/env python3
+"""
+MIT License
 
-#
-# MIT License
-#
-# Copyright (c) 2020-2022 EntySec
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
+Copyright (c) 2020-2022 EntySec
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 
 import paramiko
-
+import threading
 from time import sleep as ssh_delay
-from threading import Thread as ssh_thread
+from typing import Union
 
 from .badges import Badges
 
 
 class Shreder(Badges):
-    password = None
+    """ Main class of shreder module.
 
-    def connect(self, host, port, username, password):
+    This main class of shreder module is intended for providing
+    implementations of SSH protocol brute-forcing methods.
+    """
+
+    password = ""
+
+    def connect(self, host: str, port: int, username: str, password: str) -> None:
+        """ Connect to the remote host.
+
+        :param str host: host to connect to
+        :param int port: port to connect to
+        :param str username: username to authenticate with
+        :param str password: password to authenticate with
+        :return None: None
+        """
+
         ssh = paramiko.client.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
@@ -46,7 +59,19 @@ class Shreder(Badges):
             return
         ssh.close()
 
-    def brute(self, host, port, username, dictionary, delay=0.1):
+    def brute(self, host: str, port: int, username: str, dictionary: str,
+              delay: Union[float, int] = 0.1) -> str:
+        """ Start brute-forcing threads for the specified host and get password.
+
+        :param str host: host to brute-force
+        :param int port: port to brute-force
+        :param str username: username to brute-force with
+        :param str dictionary: name of the file containing passwords to
+        bruteforce with
+        :param Union[float, int] delay: delay between each thread
+        :return str: obtained password
+        """
+
         with open(dictionary, 'r') as f:
             threads = list()
             lines = f.read().split('\n')
@@ -54,13 +79,13 @@ class Shreder(Badges):
             for password in lines:
                 if password.strip():
                     threads.append(
-                        ssh_thread(
+                        threading.Thread(
                             target=self.connect,
                             args=[host, port, username, password]
                         )
                     )
 
-            line = "/-\|"
+            line = "/-\\|"
             counter = 0
             tried = 1
 
@@ -77,6 +102,7 @@ class Shreder(Badges):
 
                     counter += 1
                     tried += 1
+
             self.print_empty(end='')
 
             for thread in threads:
--- shreder/cli.py
+++ shreder/cli.py
@@ -1,39 +1,47 @@
-#!/usr/bin/env python3
+"""
+MIT License
 
-#
-# MIT License
-#
-# Copyright (c) 2020-2022 EntySec
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
+Copyright (c) 2020-2022 EntySec
 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import argparse
 import os
 import time
-import argparse
 
 from .__main__ import Shreder
 from .badges import Badges
 
 
 class ShrederCLI(Shreder, Badges):
-    description = "Shreder is a powerful multi-threaded SSH protocol password brute-force tool."
+    """ Subclass of shreder module.
+
+    This subclass of shreder module is intended for providing
+    command-line interface for Shreder.
+    """
+
+    description = (
+        'Shreder is a powerful multi-threaded SSH protocol'
+        'password brute-force tool.'
+    )
+
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('target')
     parser.add_argument('-p', '--port', dest='port', help='SSH port.')
@@ -42,7 +50,12 @@ class ShrederCLI(Shreder, Badges):
     parser.add_argument('-d', '--delay', dest='delay', help='Delay between login attempts.')
     args = parser.parse_args()
 
-    def start(self):
+    def start(self) -> None:
+        """ Main command-line arguments handler.
+
+        :return None: None
+        """
+
         if self.args.target and self.args.username and self.args.list:
             if not os.path.exists(self.args.list):
                 self.print_error(f"List file: {self.args.list}: does not exist!")
@@ -70,6 +83,12 @@ class ShrederCLI(Shreder, Badges):
         else:
             self.parser.print_help()
 
-def main():
+
+def main() -> None:
+    """ Shreder command-line interface.
+
+    :return None: None
+    """
+
     cli = ShrederCLI()
     cli.start()
