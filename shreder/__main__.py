44abd81d5a8afc95d79ad4fdebacaf8b776d4638
Ivan Nikolsky <enty8080@gmail.com>
2023-03-09 15:24:10 +0300

    Add files via upload

--- shreder/__main__.py
+++ shreder/__main__.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2020-2022 EntySec
+Copyright (c) 2020-2023 EntySec
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -37,7 +37,7 @@ class Shreder(Badges):
     implementations of SSH protocol brute-forcing methods.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
 
         self.password = ""
--- shreder/cli.py
+++ shreder/cli.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2020-2022 EntySec
+Copyright (c) 2020-2023 EntySec
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -37,7 +37,7 @@ class ShrederCLI(Shreder, Badges):
     command-line interface for Shreder.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
 
         self.description = (
